0ffbf704f533110cedde349e7e8bb2d39a8f80b7
Abdul Rehman Khan <rehman.khan@ozu.edu.tr>
2024-12-10 18:27:10 +0300

    Added code for Book class

--- src/BookManagement/Book.java
+++ src/BookManagement/Book.java
@@ -1,4 +1,33 @@
 package BookManagement;
 
 public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(int id, String title, String author, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    // Getters and setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { isAvailable = available; }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", available=" + isAvailable + "]";
+    }
 }
