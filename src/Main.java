62cb642475a86f4d047b7820ca5a756ba3a1fbfb
AYESH-pixel <ayesha.tehseen@ozu.edu.tr>
2024-12-10 18:53:45 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,19 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+    Loan loan1 = new Loan(1, 101, 1, LocalDate.now(), LocalDate.now().plusDays(14));
+        Loan loan2 = new Loan(2, 102, 2, LocalDate.now(), LocalDate.now().plusDays(7));
+
+        // Display loan details
+        System.out.println(loan1);
+        System.out.println(loan2);
+
+        // Mark the first loan as returned
+        loan1.returnBook();
+
+        // Display the updated loan details
+        System.out.println(loan1);
+    }
+}
 
 }
