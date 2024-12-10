d8d670e9bd20cc390643912c621d26173869261a
felix <scheffel-felix@t-online.de>
2024-12-10 18:23:28 +0300

    add Admin class

--- src/UserManagement/Admin.java
+++ src/UserManagement/Admin.java
@@ -1,4 +1,20 @@
 package UserManagement;
 
-public class Admin {
+import BookManagement.Book;
+import BookManagement.Catalog;
+
+public class Admin extends User{
+    public Admin(int id, String username, String password, String role) {
+        super(id, username, password, role);
+    }
+
+    public void addBook(Catalog catalog, Book book) {
+        catalog.addBook(book);
+        System.out.println("Book added: " + book.getTitle());
+    }
+
+    public void removeBook(Catalog catalog, int bookId) {
+        catalog.removeBook(bookId);
+        System.out.println("Book with ID " + bookId + " removed.");
+    }
 }
