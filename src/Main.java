ece0ba6068dcd2ab0870a0c37bd025ae57464ce0
leylaisa <leilaisabalaeva123@gmail.com>
2024-12-10 18:57:55 +0300

    Fixed a small issue in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
+import LoanManagement.Loan;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-    Loan loan1 = new Loan(1, 101, 1, LocalDate.now(), LocalDate.now().plusDays(14));
+        Loan loan1 = new Loan(1, 101, 1, LocalDate.now(), LocalDate.now().plusDays(14));
         Loan loan2 = new Loan(2, 102, 2, LocalDate.now(), LocalDate.now().plusDays(7));
 
         // Display loan details
@@ -25,6 +19,4 @@ public class Main {
         // Display the updated loan details
         System.out.println(loan1);
     }
-}
-
 }
